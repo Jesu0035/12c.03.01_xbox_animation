0840485bd426e09b7a1fd73c6e2871509ff22ca7
noeldj <noel0045@stud.kea.dk>
2021-03-24 13:50:01 +0100

    linked all pages and corrected some file names

---
+++ javascript/start.js
@@ -0,0 +1,34 @@
+      document.body.addEventListener('click', () => {
+            const overlay = document.querySelector('.overlay')
+            document.querySelector('.overlay').style.background = 'white';
+
+
+
+            document.querySelector('.controller').classList.add('z-index')
+            setTimeout(() => {
+                document.querySelector('.stage').remove()
+                logo = document.createElement('img')
+                logo.src = '../images/home-page-images/logo.svg'
+                logo.classList.add('logo')
+                document.querySelector('main').appendChild(logo)
+            }, 1000);
+
+            setTimeout(() => {
+                window.location.assign("home.html");
+            }, 4000);
+
+
+        })
+
+        const controller = document.querySelector('.controller')
+
+        controller.addEventListener('mouseenter', () => {
+            console.log('mouseenter')
+            controller.style.transform = 'rotate(20deg)'
+            controller.classList.remove('hover')
+        })
+
+        controller.addEventListener('mouseleave', () => {
+            controller.style.transform = 'rotate(0)'
+            controller.classList.add('hover')
+        })
