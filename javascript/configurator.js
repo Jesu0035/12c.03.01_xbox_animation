4a305f23bb5c2db73523e76cdacb0f67a1846abc
noeldj <noel0045@stud.kea.dk>
2021-03-24 18:44:02 +0100

    changes

--- javascript/configurator.js
+++ javascript/configurator.js
@@ -2,11 +2,11 @@
 
 // The model of all features
 const features = {
-  Holder_phone: false,
-  led: false,
-  propeller: false,
-  shield_black: false,
-  solarfan: false
+    Holder_phone: false,
+    led: false,
+    propeller: false,
+    shield_black: false,
+    solarfan: false
 };
 
 let onlyOne = true
@@ -18,187 +18,198 @@ let elementToPaint = ''
 
 async function start() {
 
-  let response = await fetch('../svg/MasterNoelB-01.svg')
-  let mySvgData = await response.text();
-  document.querySelector('.main-product').innerHTML = mySvgData;
+    let response = await fetch('../svg/MasterNoelB-01.svg')
+    let mySvgData = await response.text();
+    document.querySelector('.main-product').innerHTML = mySvgData;
 
-  document.querySelectorAll(".option").forEach(option => option.addEventListener("click", toggleOption));
+    document.querySelectorAll(".option").forEach(option => option.addEventListener("click", toggleOption));
 
-  startManiputaltionSvg();
+    startManiputaltionSvg();
 }
 
 function startManiputaltionSvg() {
-  document.querySelectorAll(".g_to_interact_with").forEach(eachG => {
-    console.log(eachG)
-    eachG.style.fill = 'purple'
-    eachG.addEventListener("click", theClick);
-    eachG.addEventListener("mouseover", () => {
-      console.log(2)
-    });
-    eachG.addEventListener("mouseout", theMouseout);
+    document.querySelectorAll(".g_to_interact_with").forEach(eachG => {
+        eachG.style.fill = 'purple'
+        eachG.addEventListener("click", theClick);
+        eachG.addEventListener("mouseover", () => {
+            console.log(2)
+        });
+        eachG.addEventListener("mouseout", theMouseout);
 
-    document.querySelectorAll(".option-color").forEach(each_btn => {
-      each_btn.addEventListener("click", colorClick);
+        document.querySelectorAll(".option-color").forEach(each_btn => {
+            each_btn.addEventListener("click", colorClick);
 
 
-    });
-  })
+
+
+
+        })
+
+        document.querySelector('input[type=color] ').addEventListener('input', (e) => {
+            eachG.style.fill = e.target.value
+        })
+
+        function colorClick() {
+            elementToPaint = this.lastElementChild.style.backgroundColor
+            if (elementToPaint != undefined) {
+                eachG.style.fill = this.lastElementChild.style.backgroundColor;
+                elementToPaint = this.lastElementChild.style.backgroundColor;
+                console.log(eachG)
+            }
+
+        }
+
+
+
+    })
 
 }
 //this function make first click grey
 function theClick(e) {
-  console.log('click')
-  console.log(e.target)
-  console.log(this)
-  console.log(elementToPaint)
-  this.style.fill = elementToPaint
+    console.log('click')
+    console.log(e.target)
+    console.log(this)
+    console.log(elementToPaint)
+    this.style.fill = elementToPaint
 }
 /* this function make the lines blue so we can select the place  */
 function theMouseover() {
-  console.log(this)
-  this.style.stroke = "blue"
+    console.log(this)
+    this.style.stroke = "blue"
 }
 /* this function makes desapear the blue stroke when we move the mouse */
 function theMouseout() {
-  console.log(this)
-  this.style.stroke = "none"
+    console.log(this)
+    this.style.stroke = "none"
 }
 
-function colorClick() {
-  elementToPaint = this.lastElementChild.style.backgroundColor
-  if (elementToPaint != undefined) {
-    document.querySelector('.selected-color').style.background = this.lastElementChild.style.backgroundColor;
-    elementToPaint = this.lastElementChild.style.backgroundColor;
-  }
-
-}
 
 
 function toggleOption(event) {
-  const target = event.currentTarget;
-  const feature = target.dataset.feature;
+    const target = event.currentTarget;
+    const feature = target.dataset.feature;
 
-  // TODO: Toggle feature in "model"
-  features[feature] = !features[feature];
+    // TODO: Toggle feature in "model"
+    features[feature] = !features[feature];
 
-  if (features[feature] === true) {
+    if (features[feature] === true) {
 
-    const category = target.parentElement.dataset.category
-    const childrenElements = document.querySelector(`ul [data-feature="${target.dataset.feature}"`)
-    const chosen = target.parentElement.querySelector('.chosen')
+        const category = target.parentElement.dataset.category
+        const childrenElements = document.querySelector(`ul [data-feature="${target.dataset.feature}"`)
+        const chosen = target.parentElement.querySelector('.chosen')
 
 
-    if (target.parentElement.dataset.category == 'arrowkeys') {
+        if (target.parentElement.dataset.category == 'arrowkeys') {
 
-      if (chosen) {
-        alert('You can only pick one ' + target.parentElement.dataset.category + '. Deselect current element.')
-      } else {
-        addFeature(target)
-      }
+            if (chosen) {
+                alert('You can only pick one ' + target.parentElement.dataset.category + '. Deselect current element.')
+            } else {
+                addFeature(target)
+            }
 
-    } else if (target.parentElement.dataset.category == 'thumbsticks') {
+        } else if (target.parentElement.dataset.category == 'thumbsticks') {
 
-      if (chosen) {
-        alert('You can only pick one ' + target.parentElement.dataset.category + '. Deselect current element.')
-      } else {
-        addFeature(target)
-        copyImage(target)
-      }
+            if (chosen) {
+                alert('You can only pick one ' + target.parentElement.dataset.category + '. Deselect current element.')
+            } else {
+                addFeature(target)
+                copyImage(target)
+            }
+
+        } else {
+            addFeature(target)
+        }
+
+
 
     } else {
-      addFeature(target)
-    }
+        removeFeature(target)
+        if (document.querySelector('#selected ul')) {
 
-
-
-  } else {
-    removeFeature(target)
-    if (document.querySelector('#selected ul')) {
+        }
 
     }
 
-  }
+    function copyImage(target) {
 
-  function copyImage(target) {
+        const previewImage = document.querySelector(`#product-preview [data-feature=${target.dataset.feature}`)
+        const copy = previewImage.cloneNode(true)
+        copy.classList.add('copy')
+        copy.id = 'copy'
 
-    const previewImage = document.querySelector(`#product-preview [data-feature=${target.dataset.feature}`)
-    const copy = previewImage.cloneNode(true)
-    copy.classList.add('copy')
-    copy.id = 'copy'
+        console.log(copy)
+        document.querySelector('.img-container').appendChild(copy)
+        copy.classList.remove('hide')
+    }
 
-    console.log(copy)
-    document.querySelector('.img-container').appendChild(copy)
-    copy.classList.remove('hide')
-  }
+    function addFeature(target) {
+        //Select target and add chosen class
+        target.classList.add("chosen")
 
-  function addFeature(target) {
-    //Select target and add chosen class
-    target.classList.add("chosen")
+        //Remove the hide class
+        document.querySelector(`[data-feature="${feature}"`).classList.remove("hide")
+        //Create new featureElement and add it to the list
+        const newfeatureElement = createFeatureElement(feature)
+        document.querySelector("#selected ul").appendChild(newfeatureElement)
+        // feature added
 
-    //Remove the hide class
-    document.querySelector(`[data-feature="${feature}"`).classList.remove("hide")
-    //Create new featureElement and add it to the list
-    const newfeatureElement = createFeatureElement(feature)
-    document.querySelector("#selected ul").appendChild(newfeatureElement)
-    // feature added
+        //FLIP
+        const start = target.lastElementChild.getBoundingClientRect();
+        const end = newfeatureElement.getBoundingClientRect();
 
-    //FLIP
-    const start = target.lastElementChild.getBoundingClientRect();
-    const end = newfeatureElement.getBoundingClientRect();
+        const diffx = start.x - end.x + "px";
+        const diffy = start.y - end.y + "px";
 
-    const diffx = start.x - end.x + "px";
-    const diffy = start.y - end.y + "px";
+        newfeatureElement.style.setProperty("--diffx", diffx);
+        newfeatureElement.style.setProperty("--diffy", diffy);
 
-    newfeatureElement.style.setProperty("--diffx", diffx);
-    newfeatureElement.style.setProperty("--diffy", diffy);
+        //Animation feature in
+        newfeatureElement.classList = "animate-feature-in";
+    }
 
-    //Animation feature in
-    newfeatureElement.classList = "animate-feature-in";
-  }
+    function removeFeature(item) {
+        item.classList.remove("chosen");
+        console.log(item)
 
-  function removeFeature(item) {
-    item.classList.remove("chosen");
-    console.log(item)
+        console.log(document.querySelector(`[data-feature="${feature}"`))
 
-    console.log(document.querySelector(`[data-feature="${feature}"`))
+        const theFeatureElement = document.querySelector(`#selected [data-feature="${feature}"]`);
 
-    const theFeatureElement = document.querySelector(`#selected [data-feature="${feature}"]`);
+        const end = theFeatureElement.getBoundingClientRect();
+        const start = item.lastElementChild.getBoundingClientRect();
 
-    const end = theFeatureElement.getBoundingClientRect();
-    const start = item.lastElementChild.getBoundingClientRect();
+        const diffx = start.x - end.x + "px";
+        const diffy = start.y - end.y + "px";
 
-    const diffx = start.x - end.x + "px";
-    const diffy = start.y - end.y + "px";
+        theFeatureElement.style.setProperty("--diffx", diffx);
+        theFeatureElement.style.setProperty("--diffy", diffy);
 
-    theFeatureElement.style.setProperty("--diffx", diffx);
-    theFeatureElement.style.setProperty("--diffy", diffy);
+        theFeatureElement.offsetHeight;
 
-    theFeatureElement.offsetHeight;
-
-    //Animation feature out
-    theFeatureElement.classList = "animate-feature-out";
+        //Animation feature out
+        theFeatureElement.classList = "animate-feature-out";
 
 
-    //when animation is complete, remove featureElement from the DOM
-    theFeatureElement.addEventListener("animationend", function () {
-      theFeatureElement.remove();
-      //Choose the feature element and hide it
-      document.querySelector(`[data-feature=${feature}`).classList.add("hide");
+        //when animation is complete, remove featureElement from the DOM
+        theFeatureElement.addEventListener("animationend", function () {
+            theFeatureElement.remove();
+            //Choose the feature element and hide it
+            document.querySelector(`[data-feature=${feature}`).classList.add("hide");
 
-      const copy = document.querySelector('.copy')
+            const copy = document.querySelector('.copy')
 
-      copy.classList.add('hide')
+            copy.classList.add('hide')
 
-      console.log(copy)
+            console.log(copy)
 
-      copy.addEventListener('transitionend', () => {
-        copy.remove()
-      })
+            copy.addEventListener('transitionend', () => {
+                copy.remove()
+            })
 
 
 
-    });
-  }
+        });
+    }
 
 
 
@@ -208,58 +219,58 @@ function toggleOption(event) {
 
 // Create featureElement to be appended to #selected ul - could have used a <template> instead
 function createFeatureElement(feature) {
-  //Create an li element and add feature img into it
-  const li = document.createElement("li");
-  li.dataset.feature = feature;
+    //Create an li element and add feature img into it
+    const li = document.createElement("li");
+    li.dataset.feature = feature;
 
-  const img = document.createElement("img");
-  img.src = `images/configurator-images/${feature}.png`;
-  img.alt = capitalize(feature);
+    const img = document.createElement("img");
+    img.src = `images/configurator-images/${feature}.png`;
+    img.alt = capitalize(feature);
 
-  //Add the li element
-  li.append(img);
+    //Add the li element
+    li.append(img);
 
-  return li;
+    return li;
 }
 
 function capitalize(text) {
-  return text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase();
+    return text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase();
 }
 
 
 
 
 document.querySelectorAll('.categories [data-category]').forEach(tab => {
-  tab.addEventListener('click', (e) => {
+    tab.addEventListener('click', (e) => {
 
 
 
-    document.querySelectorAll('.categories [data-category]').forEach(item => {
-      item.classList.remove('category-active')
+        document.querySelectorAll('.categories [data-category]').forEach(item => {
+            item.classList.remove('category-active')
+        })
+
+
+
+
+        const category = e.target.dataset.category
+        e.target.classList.add('category-active')
+
+        const currentTabs = document.querySelectorAll('.tab')
+        const currentTab = document.querySelector(`.tabs [data-category="${category}"]`)
+
+        currentTabs.forEach(item => {
+
+            item.classList.remove('active')
+        })
+
+        currentTab.classList.add('active')
     })
-
-
-
-
-    const category = e.target.dataset.category
-    e.target.classList.add('category-active')
-
-    const currentTabs = document.querySelectorAll('.tab')
-    const currentTab = document.querySelector(`.tabs [data-category="${category}"]`)
-
-    currentTabs.forEach(item => {
-
-      item.classList.remove('active')
-    })
-
-    currentTab.classList.add('active')
-  })
 })
 
 document.querySelectorAll('.option-color').forEach(color => {
 
 
-  color.firstElementChild.textContent = capitalize(color.dataset.color)
-  color.lastElementChild.style.background = color.dataset.color
-  color.addEventListener('click', (e) => {})
+    color.firstElementChild.textContent = capitalize(color.dataset.color)
+    color.lastElementChild.style.background = color.dataset.color
+    color.addEventListener('click', (e) => {})
 })
