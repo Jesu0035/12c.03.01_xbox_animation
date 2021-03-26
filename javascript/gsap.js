b2c3ccc3db613c50f13b8bf33dab5edc612beb85
noeldj <noel0045@stud.kea.dk>
2021-03-26 07:23:10 +0100

    commit

--- javascript/gsap.js
+++ javascript/gsap.js
@@ -20,14 +20,6 @@ gsap.from(".wrapper", {
     delay: 1
 });
 
-gsap.from(".header", {
-    display: 'none',
-    opacity: 0,
-    height: 0,
-    padding: 0,
-    duration: 1,
-    delay: 1
-});
 
 
 let t1 = gsap.timeline({
@@ -73,14 +65,14 @@ gsap.from("#first img", {
     rotation: -20
 });
 
-gsap.to("#second .watermark", {
+gsap.from("#second .watermark", {
     scrollTrigger: {
         trigger: "#second",
-        start: "-40% 20%",
+        start: "0% 100%",
         scrub: 3
     },
     duration: 2,
-    x: '20vw'
+    x: '-200vw'
 });
 
 gsap.from("#third .watermark", {
